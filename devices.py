dfa35475abf3c4cc2e4784b7809c6bbd6cffb8db
Nia-Ashley <niaashleyharris@gmail.com>
2026-09-11 16:04:59 -0500

    task 1: list devices

--- devices.py
+++ devices.py
@@ -5,3 +5,11 @@ readings = [
     {"name": "fridge",     "room": "kitchen", "temp": 4.2,  "online": False},
     {"name": "patio",      "room": "outside", "temp": 29.8, "online": True},
 ]
+
+#write a function called list_devices(devices) — print each device's name and temperature using the data in the list called readings
+def list_devices(devices):
+    for device in devices:
+        print(f"Device Name: {device['name']}, Temperature: {device['temp']}°C")
+
+
+list_devices(readings)
